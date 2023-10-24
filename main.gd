2250873e219d5d4cfaaa48c76da5f91ec84e9564
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-10-23 20:29:07 -0400

    Set up unique collision layers for bullets

--- main.gd
+++ main.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 func _ready():
 	$Ship.bullet_fired.connect(on_player_fired_bullet)
+	$Ship2.bullet_fired.connect(on_player_fired_bullet)
 
-func on_player_fired_bullet(bullet_scene, direction, location):
+func on_player_fired_bullet(bullet_scene, direction, location, bullet_layer):
 	var bullet = bullet_scene.instantiate()
 	add_child(bullet)
 	bullet.global_position = location
 	bullet.set_direction(direction) 
+	bullet.set_collision_layer(bullet_layer)
+	bullet.set_collision_mask(bullet_layer)
--- ship/ship.gd
+++ ship/ship.gd
@@ -6,17 +6,18 @@ extends CharacterBody2D
 @export var ROTATION_SPEED = 270 #Degrees per second. Multiply by delta
 @export var SHIP_SPRITE : Texture2D = preload("res://ship/ship_sprite.png")
 @export var hp = 100
-@export var thrust_string = "p1_thrust"
-@export var rotate_clockwise_string = "p1_rotate_clockwise"
-@export var rotate_anticlockwise_string = "p1_rotate_anticlockwise"
-@export var fire_string = "p1_fire"
+@export var THRUST_STRING = "p1_thrust"
+@export var ROTATE_CLOCKWISE_STRING = "p1_rotate_clockwise"
+@export var ROTATE_ANTICLOCKWISE_STRING = "p1_rotate_anticlockwise"
+@export var FIRE_STRING = "p1_fire"
+@export var BULLET_LAYER = 2
 
 var current_speed = 0
 var thrust_direction = transform.x
 
 var BULLET_SCENE = preload("res://bullet/bullet.tscn")
 
-signal bullet_fired(bullet, direction, location)
+signal bullet_fired(bullet, direction, location, bullet_layer)
 
 func _ready():
 	$Sprite2D.texture = SHIP_SPRITE
@@ -27,12 +28,12 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _input(event):
-	if event.is_action_pressed(fire_string):
-		bullet_fired.emit(BULLET_SCENE, transform.x, global_position)
+	if event.is_action_pressed(FIRE_STRING):
+		bullet_fired.emit(BULLET_SCENE, transform.x, global_position, BULLET_LAYER)
 	
 func get_input(delta):
 	#-----------Thrust-------------------
-	if Input.is_action_pressed(thrust_string):
+	if Input.is_action_pressed(THRUST_STRING):
 		current_speed += ACCELERATION * delta
 		thrust_direction = transform.x
 	else:
@@ -41,7 +42,7 @@ func get_input(delta):
 	current_speed = clamp(current_speed, 0, TOP_SPEED)
 	
 	#------------Rotation-----------------
-	var rotation_direction = Input.get_axis(rotate_anticlockwise_string, rotate_clockwise_string)
+	var rotation_direction = Input.get_axis(ROTATE_ANTICLOCKWISE_STRING, ROTATE_CLOCKWISE_STRING)
 	rotation_degrees += rotation_direction * ROTATION_SPEED * delta
 	
 func on_hit(damage):
