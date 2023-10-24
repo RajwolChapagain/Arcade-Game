cca56328f96790c5fcbf63c17bc3d2c420136757
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-10-23 20:33:27 -0400

    Make bullet damage ship on hit

--- bullet/bullet.gd
+++ bullet/bullet.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
 @export var SPEED = 4000
+@export var DAMAGE = 10
+
 var direction = transform.x
 
 func _physics_process(delta):
-	move_and_collide(direction * SPEED * delta)
+	var collision = move_and_collide(direction * SPEED * delta)
+	
+	if collision != null:
+		if collision.get_collider().has_method("on_hit"):
+			collision.get_collider().on_hit(DAMAGE)
+			queue_free()
 
 func set_direction(new_direction):
 	direction = new_direction
