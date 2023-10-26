8e67dce394f8557c698ac888854fb91b3c2a5c80
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-10-25 22:50:31 -0400

    Fix health bar not updating correctly on super hit

--- UI/hud.gd
+++ UI/hud.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func decrease_p1_health(damage):
-	$HealthBar.value -= 10
+	$HealthBar.value -= damage
 	
 func decrease_p2_health(damage):
-	$HealthBar2.value -= 10
+	$HealthBar2.value -= damage
