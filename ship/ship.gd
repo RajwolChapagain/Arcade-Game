e8d346c311daa1c9d5b22ce7ac6788f52548b818
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-11-06 18:13:09 -0500

    Implement a basic shield

--- ship/ship.gd
+++ ship/ship.gd
@@ -11,9 +11,11 @@ extends CharacterBody2D
 @export var DOWN_STRING = "p1_down"
 @export var FIRE_STRING = "p1_fire"
 @export var SUPER_STRING = "p1_super"
+@export var SHIELD_STRING = "p1_shield"
 @export var BULLET_LAYER = 2
 @export var SPEED_DECREASE_ON_FIRE = 250
 @export var SUPER_DURATION = 1
+@export var SHIELD_DURATION = 2
 @export var super_percentage = 0:
 	get:
 		return super_percentage
@@ -24,7 +26,9 @@ extends CharacterBody2D
 @export var TIME_TO_FILL_SUPER = 10
 
 var thrust_speed = 0
-var frozen = false
+var frozen = false#might be obsolete
+var shield_button_is_pressed = false
+var shield_is_active = false
 
 var BULLET_SCENE = preload("res://bullet/bullet.tscn")
 var SUPER_SCENE = preload("res://ship/super.tscn")
@@ -39,6 +43,7 @@ signal player_entered_screen
 func _ready():
 	$Sprite2D.texture = SHIP_SPRITE
 	$SuperTimer.wait_time = SUPER_DURATION
+	$ShieldTimer.wait_time = SHIELD_DURATION
 	
 func _process(delta):
 	super_percentage += delta * 100 / TIME_TO_FILL_SUPER
@@ -55,14 +60,19 @@ func _input(event):
 	
 	if event.is_action_pressed(SUPER_STRING) and super_percentage == 100:
 		super_percentage = 0
-		var super_instance = SUPER_SCENE.instantiate()
-		add_child(super_instance)
-		super_instance.collision_layer = BULLET_LAYER
-		super_instance.collision_mask = BULLET_LAYER
-		$SuperTimer.start()
-		freeze()
+		if shield_button_is_pressed:
+			$ShieldTimer.start()
+			activate_shield()
+		else:
+			$SuperTimer.start()
+			var super_instance = SUPER_SCENE.instantiate()
+			add_child(super_instance)
+			super_instance.collision_layer = BULLET_LAYER
+			super_instance.collision_mask = BULLET_LAYER
+			freeze()
 	
 func get_input(delta):
+	shield_button_is_pressed = Input.is_action_pressed(SHIELD_STRING)
 	var input_direction = Input.get_vector(LEFT_STRING, RIGHT_STRING, UP_STRING, DOWN_STRING)
 
 	if input_direction.length() != 0:
@@ -104,3 +114,15 @@ func _on_visibility_notifier_screen_exited():
 func _on_visibility_notifier_screen_entered():
 	#player_entered_screen.emit()
 	pass
+	
+func activate_shield():
+	$Shield.visible = true
+	shield_is_active = true
+
+func deactivate_shield():
+	$Shield.visible = false
+	shield_is_active = false
+	
+
+func _on_shield_timer_timeout():
+	deactivate_shield()
