760ff628a079fd5a3e9144f27b09b3d7abc6c915
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-11-19 20:31:21 -0500

    Give rotation property to bullet ring

--- bullet/bullet_ring.gd
+++ bullet/bullet_ring.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const RADIUS = 100
+const angular_velocity = 360
 
 func _ready():
 	var angle = 2 * PI / get_child_count()
@@ -10,3 +11,6 @@ func _ready():
 	for child in get_children():
 		child.position = distance_vector.rotated(current_angle)
 		current_angle += angle
+
+func _physics_process(delta):
+	rotation_degrees += angular_velocity * delta
