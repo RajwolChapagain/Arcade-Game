e28c3adc6bd7d85b5691e4a1923d75d92ee74128
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-11-27 15:17:30 -0500

    Make bullet disappear on any collision

--- bullet/bullet.gd
+++ bullet/bullet.gd
@@ -13,7 +13,7 @@ func _physics_process(delta):
 	if collision != null:
 		if collision.get_collider().has_method("on_hit"):
 			collision.get_collider().on_hit(DAMAGE)
-			queue_free()
+		queue_free()
 
 func set_direction(new_direction):
 	direction = new_direction
