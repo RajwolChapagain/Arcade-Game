17f7baf969312011ce2a3e8e911136d3869991a7
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-11-28 21:27:20 -0500

    Revert spawner to normal

--- powerups/spawner.gd
+++ powerups/spawner.gd
@@ -5,7 +5,7 @@ extends Node2D
 signal object_spawned(object)
 
 func _on_timer_timeout():
-	var object = objects[4].instantiate()
+	var object = objects.pick_random().instantiate()
 	$Path2D/PathFollow2D.progress_ratio = randf()
 	object.global_position = $Path2D/PathFollow2D.position
 
