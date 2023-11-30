86f149291ebf2e0ebc97a6afbf4b959de4d60efa
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-11-30 13:46:33 -0500

    Add basic script to main menu

---
+++ UI/main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@export var ships : Array
+var left_pointer = 1
+var right_pointer = 1
+
+func _ready():
+	pass
+
+func _input(event):
+	if event.is_action_pressed("p1_left"):
+		$Selection/LeftItems/LeftLeftArrow.visible = false
+	if event.is_action_pressed("p1_right"):
+		$Selection/LeftItems/LeftLeftArrow.visible = false
+	if event.is_action_pressed("p1_up"):
+		$Selection/LeftItems/LeftLeftArrow.visible = false
+	if event.is_action_pressed("p1_down"):
+		$Selection/LeftItems/LeftLeftArrow.visible = false
