c7b1710de9b6014f62ecc2b526387c5323d210fe
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-12-01 13:55:14 -0500

    Update main_menu.gd

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,18 +1,27 @@
 extends Control
 
 @export var ships : Array
-var left_pointer = 1
-var right_pointer = 1
+var left_pointer = 1:
+	get:
+		return left_pointer
+	set(value):
+		left_pointer = clamp(value, 0, ships.size() - 1)
+var right_pointer = 1:
+	get:
+		return right_pointer
+	set(value):
+		right_pointer = clamp(value, 0, ships.size() - 1)
 
 func _ready():
 	pass #▶Initialize sprite based on pointers
 
 func _input(event):
 	if event.is_action_pressed("p1_left"):
-		$Selection/LeftItems/LeftLeftArrow.visible = false
+		left_pointer -= 1
 	if event.is_action_pressed("p1_right"):
-		$Selection/LeftItems/LeftLeftArrow.visible = false
-	if event.is_action_pressed("p1_up"):
-		$Selection/LeftItems/LeftLeftArrow.visible = false
-	if event.is_action_pressed("p1_down"):
-		$Selection/LeftItems/LeftLeftArrow.visible = false
+		left_pointer += 1
+	if event.is_action_pressed("p2_left"):	
+		right_pointer -= 1
+	if event.is_action_pressed("p2_right"):
+		right_pointer += 1
+	
