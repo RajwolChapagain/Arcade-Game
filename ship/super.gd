b6a54784a85b6775226bf389504ebf5d277d0794
Rajwol Chapagain <rajwolchapagain@gmail.com>
2023-12-19 17:51:39 -0600

    Make super not damage parent ship

--- ship/super.gd
+++ ship/super.gd
@@ -5,4 +5,7 @@ extends Area2D
 func _physics_process(delta):
 	for body in get_overlapping_bodies():
 		if body.has_method("on_hit"):
+			if body == get_parent(): #Don't damage the ship that fired it
+				continue
+				
 			body.on_hit(DAMAGE * delta)
