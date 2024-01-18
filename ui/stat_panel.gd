ae4f0f3b80dcc4a5623d9a7f0c9707b86877add1
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-01-17 22:19:29 -0500

    Set up tween function of stats_panel

--- ui/stat_panel.gd
+++ ui/stat_panel.gd
@@ -1,4 +1,15 @@
 extends NinePatchRect
-
-func tween_to_stat():
-	pass
+	
+func tween_to_stats(stats : ShipStats):
+	var tween_time = 0.5
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property($Bars/Toughness, "value", stats.toughness, tween_time)
+	tween.tween_property($Bars/Damage, "value", stats.damage, tween_time)
+	tween.tween_property($Bars/RechargeRate, "value", stats.recharge_rate, tween_time)
+	tween.tween_property($Bars/TopSpeed, "value", stats.top_speed, tween_time)
+	tween.tween_property($Bars/Acceleration, "value", stats.acceleration, tween_time)	
+	
+	
+	
