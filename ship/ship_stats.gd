0961e4fddcf5fe4f91ecd12ad6ccebb40b0712b4
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-01-20 21:53:12 -0500

    Tweak ship stats

--- ship/ship_stats.gd
+++ ship/ship_stats.gd
@@ -2,7 +2,7 @@ extends Resource
 class_name ShipStats
 
 @export var toughness : int = 100
-@export var damage : int = 10
+@export var damage : int = 15
 @export var recharge_rate : int = 5
 @export var top_speed : int = 1500
 @export var acceleration : int = 1000
