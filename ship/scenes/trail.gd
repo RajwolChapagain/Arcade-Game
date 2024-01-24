7af0aad275ccd2ab71a467d1f23c4e04c0541671
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-01-24 11:44:42 -0500

    Implement trail functionality

--- ship/scenes/trail.gd
+++ ship/scenes/trail.gd
@@ -1,11 +1,7 @@
 extends Line2D
 
 @export var max_length = 20
-@export var distance_between_points = 2
 
 func _physics_process(delta):
-	if position.distance_to(points[points.size()- 1]) >= distance_between_points:
-		add_point(position)
-	
-	if points.size() > max_length / distance_between_points:
+	if points.size() > max_length:
 		remove_point(0)
