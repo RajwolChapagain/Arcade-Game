a6eee56166db2ffa34db5f0778cee014d73eb1bc
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-02-26 20:56:50 -0500

    Add ui folder

---
+++ ui/hud.gd
@@ -0,0 +1,57 @@
+extends Control
+	
+func initialize_max_hp_bar(player, max_hp):
+	var health_bar
+	
+	if player == 1:
+		health_bar = $HealthBar
+	elif player == 2:
+		health_bar = $HealthBar2
+		
+	health_bar.max_value = max_hp
+	health_bar.value = max_hp
+
+func set_p1_health(hp):
+	$HealthBar.value = hp
+	
+func set_p2_health(hp):
+	$HealthBar2.value = hp
+
+func update_p1_super_bar(new_percentage):
+	$SuperBar.value = new_percentage
+	
+func update_p2_super_bar(new_percentage):
+	$SuperBar2.value = new_percentage
+
+func announce_winner(player):
+	var player_name
+	
+	if player == 1:
+		player_name = "Player 1"
+	elif player == 2:
+		player_name = "Player 2"
+		
+	var announcement = player_name + " WINS!!!"
+	show_announcement_text(announcement)
+
+func hide_announcement_text():
+	$WinnerAnnouncementText.set_visible(false)	
+
+func show_announcement_text(text):
+	$WinnerAnnouncementText.set_text(text)
+	$WinnerAnnouncementText.set_visible(true)	
+
+func set_round_time(text):
+	$RoundTime.set_text(str(text))
+
+func indicate_round_won(player, victory_count):
+	if player == 1:
+		if victory_count == 1:
+			$TimerBackground/P1Round1Indicator.toggle_to_win()
+		elif victory_count == 2:
+			$TimerBackground/P1Round2Indicator.toggle_to_win()
+	if player == 2:
+		if victory_count == 1:
+			$TimerBackground/P2Round1Indicator.toggle_to_win()
+		elif victory_count == 2:
+			$TimerBackground/P2Round2Indicator.toggle_to_win()
---
+++ ui/main_menu.gd
@@ -0,0 +1,89 @@
+extends Control
+
+@export var ships_sprites : Array
+@export var ship_stats : Array ##Must follow the same order as ships_sprites
+
+var left_pointer = 1:
+	get:
+		return left_pointer
+	set(value):
+		left_pointer = clamp(value, 0, ships_sprites.size() - 1)
+		set_sprite(1, ships_sprites[left_pointer])
+		$P1Stats.tween_to_stats(ship_stats[left_pointer])
+var right_pointer = 1:
+	get:
+		return right_pointer
+	set(value):
+		right_pointer = clamp(value, 0, ships_sprites.size() - 1)
+		set_sprite(2, ships_sprites[right_pointer])		
+		$P2Stats.tween_to_stats(ship_stats[right_pointer])
+		
+var player1_inserted_coin = false
+var player2_inserted_coin = false
+var player1_ready = false
+var player2_ready = false
+var game_started = false
+
+signal both_players_ready(p1_ship, p2_ship)
+
+func _input(event):
+	if game_started:
+		return
+		
+	if event.is_action_pressed("p1_left"):
+		if player1_inserted_coin and !player1_ready:
+			left_pointer -= 1
+	if event.is_action_pressed("p1_right"):
+		if player1_inserted_coin and !player1_ready:
+			left_pointer += 1
+	if event.is_action_pressed("p2_left"):	
+		if player2_inserted_coin and !player2_ready:
+			right_pointer -= 1
+	if event.is_action_pressed("p2_right"):
+		if player2_inserted_coin and !player2_ready:
+			right_pointer += 1
+	
+	if event.is_action_pressed("p1_insert_coin"):
+		on_player_insert_coin(1)
+	if event.is_action_pressed("p2_insert_coin"):
+		on_player_insert_coin(2)
+	
+	if event.is_action_pressed("p1_fire"):
+		if player1_inserted_coin:
+			make_player_ready(1)
+	if event.is_action_pressed("p2_fire"):
+		if player2_inserted_coin:
+			make_player_ready(2)
+		
+func set_sprite(player, sprite : Texture2D):
+	var ship_sprite = $Selection/LeftItems/LeftShip if player == 1 else $Selection/RightItems/RightShip
+	ship_sprite.texture = sprite
+
+func on_player_insert_coin(player):
+	if player == 1:
+		player1_inserted_coin = true
+		player1_ready = false
+	elif player == 2:
+		player2_inserted_coin = true
+		player2_ready = false
+		
+	set_prompt(player, "Select Ship")
+	
+func make_player_ready(player):
+	if player == 1:
+		player1_ready = true
+		if player2_ready:
+			start_game()
+	elif player == 2:
+		player2_ready = true
+		if player1_ready:
+			start_game()
+	set_prompt(player, "Ready!")
+	
+func set_prompt(player, text):
+	var label = $Prompt/Label if player == 1 else $Prompt/Label2
+	label.text = text
+
+func start_game():
+	both_players_ready.emit(left_pointer, right_pointer)
+	game_started = true
